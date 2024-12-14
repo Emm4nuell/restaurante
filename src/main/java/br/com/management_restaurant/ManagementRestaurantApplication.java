e5333445ff487552c372b4181a9c9c13515a7076
Emm4nuell <eduardo.eesf@gmail.com>
2024-12-14 11:27:30 -0300

    Created project management restaurant

---
+++ src/main/java/br/com/management_restaurant/ManagementRestaurantApplication.java
@@ -0,0 +1,13 @@
+package br.com.management_restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementRestaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/management_restaurant/ManagementRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.management_restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
